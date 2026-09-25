837198f68575c2ac657e8603e110344020b8bc57
matthew-lee-rush <matthew.lee1406@gmail.com>
2026-09-25 16:08:00 +0100

    Added images to the Landing page, and applied styling to said images

--- my-react-app/src/features/quiz/screens/Landing.jsx
+++ my-react-app/src/features/quiz/screens/Landing.jsx
@@ -1,4 +1,6 @@
 import logoImage from "../../../assets/images/app-images/logo.png";
+import starClusterImg from "../../../assets/images/app-images/star-graphic-cluster.png";
+import starImg from "../../../assets/images/app-images/starGraphic.png";
 
 function Landing({ dispatch }) {
     return (
@@ -8,6 +10,11 @@ function Landing({ dispatch }) {
 
             
             <img className="logo-image" src={logoImage} alt="Logo"/>
+
+            <img className="star-image-left" src={starImg} alt="Star graphic"/>
+            <img className="star-cluster-image" src={starClusterImg} alt="Star Cluster"/>
+            <img className="star-image" src={starImg} alt="Star graphic"/>
+        
 
             <div className="landing-actions">
                 <button className="button" onClick={() => dispatch({ type: "GO_INFO" })}>
